a14907b83cc70d2d1aff61f6ede9849496261773
Gonzalo <gonen.rt@gmail.com>
2015-06-15 13:43:10 -0300

    hice la interfaz de coneccion

--- AplicacionBD2/src/vista/PanelIngreso.java
+++ AplicacionBD2/src/vista/PanelIngreso.java
@@ -4,8 +4,24 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 public class PanelIngreso extends JPanel implements ActionListener{
+	public PanelIngreso() {
+		
+		setLayout(null);
+		setVisible(true);
+		
+		JLabel titulo = new JLabel("Redis Connect");
+		titulo.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 20));
+		titulo.setBounds(190, 54, 168, 33);
+		add(titulo);
+		
+		JLabel lblUsuario = new JLabel("Usuario: ");
+		lblUsuario.setBounds(83, 155, 46, 14);
+		add(lblUsuario);
+	}
 
 	/**
 	 * 
@@ -17,5 +33,4 @@ public class PanelIngreso extends JPanel implements ActionListener{
 		// TODO Auto-generated method stub
 		
 	}
-
 }
