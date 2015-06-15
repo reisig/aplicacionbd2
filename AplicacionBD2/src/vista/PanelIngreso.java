b460c477c37d3f07a19e479319b0a80ecc7c18c1
Gonzalo <gonen.rt@gmail.com>
2015-06-15 13:07:26 -0300

    Signed-off-by: Gonzalo <gonen.rt@gmail.com>

---
+++ AplicacionBD2/src/vista/PanelIngreso.java
@@ -0,0 +1,21 @@
+package vista;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+public class PanelIngreso extends JPanel implements ActionListener{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
