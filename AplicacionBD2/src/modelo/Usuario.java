07ceb171f7a42cd7f1f0bea57ff20646ee0a0a6c
Luis Cuello <sopsiud@gmail.com>
2015-06-15 13:46:15 -0300

    agregados get y set en clase usuario, la contraseña es encriptada
    Signed-off-by: Luis Cuello <sopsiud@gmail.com>

--- AplicacionBD2/src/modelo/Usuario.java
+++ AplicacionBD2/src/modelo/Usuario.java
@@ -2,16 +2,43 @@ package modelo;
 import java.util.Date;
 import java.util.Map;
 
+import org.jasypt.util.text.BasicTextEncryptor;
+
 
 public class Usuario {
 	
+	//nombre del usuario, debe ser unico
 	private String nombre;
-	//private Password contraseña;
+	//contraseña se guarda encriptada
+	private String contraseña;
+	//instanciar como TreeMap o SortedMap
 	private Map<Date, Integer> calorias;
 	
-	//hola
-	//un cambio
-	//otro cambio
-	//cambio final
-	//cambio final 2
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getContraseña() {
+		BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
+		return textEncryptor.decrypt(contraseña);
+	}
+	
+	public void setContraseña(String contraseña) {
+		BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
+		this.contraseña = textEncryptor.encrypt(contraseña);
+	}
+	
+	public Map<Date, Integer> getCalorias() {
+		return calorias;
+	}
+	
+	public void setCalorias(Map<Date, Integer> calorias) {
+		this.calorias = calorias;
+	}
+	
+	
 }
